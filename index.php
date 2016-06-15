a67ffaa201bacbd5484558b1a22544a9f67c78c4
zealEI <zeal.shah@einfochips.com>
2016-06-15 15:07:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include("header.php");
 
 
-echo "<h1>EINFOCHIPS WORKSHOP DEMO</h1>";
+echo "<h1>EINFOCHIPS WORKSHOP DEMO....</h1>";
 echo "<h2>Running Container IP - ".$_SERVER['SERVER_ADDR']."</h2>";
 
 
